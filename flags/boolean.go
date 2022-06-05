5e7b557a909761ecddb0e0d6b3e749967e995474
Andrei Bulatov <abulatov@metaprime.ru>
2022-06-06 01:30:56 +0300

    added two flags examples

---
+++ flags/boolean.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+type Color string
+
+// https://en.wikipedia.org/wiki/ANSI_escape_code
+const (
+	ColorBlack  Color = "\u001b[30m"
+	ColorRed          = "\u001b[31m"
+	ColorGreen        = "\u001b[32m"
+	ColorYellow       = "\u001b[33m"
+	ColorBlue         = "\u001b[34m"
+	ColorReset        = "\u001b[0m"
+)
+
+func colorize(color Color, message string) {
+	fmt.Println(string(color), message, string(ColorReset))
+}
+
+func main() {
+	useColor := flag.Bool("color", false, "display colorized output")
+	flag.Parse()
+
+	if *useColor {
+		colorize(ColorYellow, "hello")
+		return
+	}
+
+	fmt.Println("hello")
+}
